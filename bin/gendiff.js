e7182d7d0c7e9fc9435030ff2a3f974ae0797b16
Afonin Andrey <andreyafonin96@gmail.com>
2023-05-29 20:41:47 +0300

    start 3 step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,23 @@
 #!/usr/bin/env node
 
 import { program  } from "commander";
+import _ from "lodash";
+import * as fs from 'fs'
+import path from "path";
+
+
+const command = (filepath1, filepath2) => {
+const path1 = path.resolve(filepath1)
+const path2 = path.resolve(filepath1)
+};
+
 
 program
-.version('0.0.1')
-.argument('<filepath1>')
-.argument('<filepath2>')
-.description('Compares two configuration files and shows a difference.')
-
-program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .action(command)
   .option('-f, --format <type>' , 'output format')
+  .helpOption('-h, --help', 'output usage information')
 
 program.parse();
