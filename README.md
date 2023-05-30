b665db2eecbce079b4c6b2826e86196cd5ab5c02
Afonin Andrey <andreyafonin96@gmail.com>
2023-05-30 13:48:51 +0300

    workflow

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/buldogic/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/buldogic/frontend-project-46/actions)
+
+
+![example workflow](https://github.com/buldogic/frontend-project-46/actions/workflows/actions-check/badge.svg)
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,23 +1,43 @@
 #!/usr/bin/env node
 
-import { program  } from "commander";
-import _ from "lodash";
-import * as fs from 'fs'
-import path from "path";
+import { program } from 'commander';
+import _ from 'lodash';
+import * as fs from 'fs';
+import path from 'path';
 
-
-const command = (filepath1, filepath2) => {
-const path1 = path.resolve(filepath1)
-const path2 = path.resolve(filepath1)
-};
 
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .action(command)
-  .option('-f, --format <type>' , 'output format')
-  .helpOption('-h, --help', 'output usage information')
-
+  .option('-f, --format <type>', 'output format')
+  .helpOption('-h, --help', 'output usage information');
 program.parse();
+
+const file1 = JSON.parse(fs.readFileSync(path.resolve('./__fixtures__', program.args[0]) , 'utf-8' ));
+const file2 = JSON.parse(fs.readFileSync(path.resolve('./__fixtures__', program.args[1]), 'utf-8'));
+
+
+const genDiff = (obj1, obj2) => {
+  const space = ' ';
+  const mergedObj = { ...obj1, ...obj2 };
+  const keys = Object.keys(mergedObj);
+  const result = _.orderBy(keys).map((key) => {
+    if (!Object.hasOwn(obj1, key)) {
+      return `${space.repeat(2)}+ ${key}: ${obj2[key]}`;
+    }
+    if (!Object.hasOwn(obj2, key)) {
+      return `${space.repeat(2)}- ${key}: ${obj1[key]}`;
+    }
+    if (obj1[key] !== obj2[key]) {
+      return `${space.repeat(2)}- ${key}: ${obj1[key]}\n  + ${key}: ${
+        obj2[key]
+      }`;
+    }
+    return `${space.repeat(4)}${key}: ${obj1[key]}`;
+  });
+  return `{\n${result.join('\n')}\n}`;
+};
+
+console.log(genDiff(file1,file2));
